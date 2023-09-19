21a89b7bf807049acaad7607c6bd60948e0af4c4
priyakumi <135395465+priyakumi@users.noreply.github.com>
2023-09-19 06:19:15 -0700

    intial code to main

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+/* TODO list in connection.js
+1. Include the module "sequelize"
+2. Include  require('dotenv').config();
+3.connection to database 
+4.exports this module as "sequelize"
+*/
+
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,15 @@
+/*  LOGIN PAGE  AND LOGOUT PAGE ROUTES*/
+ /* TODO LIST
+   1.Include module ('express').Router()
+   2.Include "user model" (it has the user data login data verifications)
+   3.Post route to the root(/) 
+   4.Post root to the login page.
+      *validate the user's login email data
+      *validate the user's password (like length)
+      *if user login data is validated
+        render a welcome message with user name,
+      else
+       render a invalid login name or password
+   5.Post route for logout page
+   6.Exports this module as router */
+
