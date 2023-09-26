0810dfb6c6ef9516de0a3ba5a56c602c25ba569a
priyakumi <135395465+priyakumi@users.noreply.github.com>
2023-09-25 21:21:57 -0700

    addedmodel

---
+++ models/index.js
@@ -0,0 +1,19 @@
+
+const expense = require('./expense');
+const User = require('./User');
+
+// Define a Driver as having many Cars, thus creating a foreign key in the `car` table
+User.hasMany(expense, {
+    foreignKey: 'uid',
+  });
+  
+  // The association can also be created from the Car side
+  expense.belongsTo(User, {
+    foreignKey: 'uid',
+  });
+  
+  module.exports = { User, expense };
+  
+
+
+
