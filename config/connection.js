95f8bdc12ae50d96f281dbcc7988d3630497d665
Fabio Silveira <fabiosilveira@Fabios-MacBook-Pro.local>
2023-09-25 20:22:49 -0700

    AddedConnection

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,16 @@
-/* TODO list in connection.js
-1. Include the module "sequelize"
-2. Include  require('dotenv').config();
-3.connection to database 
-4.exports this module as "sequelize"
-*/
-
-
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+// create connection to db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize('saversDB', 'root', '',  {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+      dialectOptions: {
+        decimalNumbers: true,
+      }
+    });
 
 module.exports = sequelize;
