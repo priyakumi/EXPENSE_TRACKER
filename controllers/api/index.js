93b3e13fd52fd56ecdb95721c9bf320d99a240c2
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-09-28 15:45:31 -0700

    fixing bugs

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userRoutes = require('./userRoutes');
-const expenseRoutes = require('./expenseRoutes');
-
-router.use('/users', userRoutes);
-router.use('/expense', expenseRoutes);
-
-module.exports = router;
