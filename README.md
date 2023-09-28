380f005b75ca5bbd3e3ec711fa808ede58220305
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-09-27 18:16:42 -0700

    Fixing bugs
    Updating

--- README.md
+++
@@ -1,1 +0,0 @@
-# TRACK_SAVER
--- middleware/sweetAlertMessages.js
+++
@@ -1,9 +0,0 @@
-const Swal = require('sweetalert2');
-
-// Middleware to set SweetAlert messages in response locals
-function setSweetAlertMessages(req, res, next) {
-  res.locals.swal = Swal;
-  next();
-}
-
-module.exports = setSweetAlertMessages;
